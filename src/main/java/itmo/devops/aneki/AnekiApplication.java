d44d5b8d3c94a8af1a51aee688bb30d23a338b58
elegantcone <elegantcone@gmail.com>
2026-02-22 18:35:36 +0300

    init commit

---
+++ src/main/java/itmo/devops/aneki/AnekiApplication.java
@@ -0,0 +1,13 @@
+package itmo.devops.aneki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnekiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itmo/devops/aneki/AnekiApplicationTests.java
@@ -0,0 +1,8 @@
+package itmo.devops.aneki;
+
+class AnekiApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
